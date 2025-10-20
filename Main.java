625b7b3770e8fec2d9e332655ebbdb318f02f129
Gustavo Henrique De Jesus <gustavohenrique17062007@gmail.com>
2025-10-20 14:49:48 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String[] args){
+        int a = 10;
+        double b = 20.5;
+        boolean c = true;
+        double resultado1;
+        double resultado2;
+
+        resultado1 = a + b;
+        System.out.println("Soma: " + resultado1);
+
+        resultado2 = a * b;
+        System.out.println("Multiplicação: " +resultado2);
+
+        c = !c;
+        System.out.println("Inverso é: " +c);
+
+    }
+
+
+
+
+
+}
