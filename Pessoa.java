21ef88a88a394610b0e43edc5da66bad2f69d4d6
Gustavo Henrique De Jesus <gustavohenrique17062007@gmail.com>
2025-10-22 11:28:38 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+
+    String nome;
+    int idade;
+
+    public void mostrarInformacoes() {
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Idade: "+idade);
+        System.out.println("");
+
+    }
+}
